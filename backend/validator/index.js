bcf2acac21957a5f421c34b4f95ee24481ca4c9f
Nazif Sahim <39175955+Sahim-Nazif@users.noreply.github.com>
2021-03-19 16:43:22 -0400

    User sign up validation added

--- backend/validator/index.js
+++ backend/validator/index.js
@@ -23,3 +23,33 @@ exports. createPostValidator=(req, res, next)=>{
 }
 
 
+exports.userSignupValidator=(req,res,next)=>{
+
+
+    req.check('name', 'Name is required').notEmpty()
+    req.check('email', 'Email is required')
+    .matches(/.+\@.+\..+/)
+    .withMessage('Email must contain @')
+    .isLength({ 
+        min:15,
+        max:200
+    })
+
+    req.check('password', 'Please provide a password').notEmpty()
+    req.check('password')
+        .isLength({min:6})
+        .withMessage('Password must be at least 6 characters')
+        .matches(/\d/)
+        .withMessage('Password must contain a number')
+
+        const errors=req.validationErrors()
+
+        //if error show the first one as it happen
+        if (errors){
+            const firstError=errors.map((error)=>error.msg)[0]
+            return res.status(400).json({error:firstError})
+        }
+        //proceed to next middle ware
+    
+        next()
+}
